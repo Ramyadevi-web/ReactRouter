d78ab2ea52e698eb75fd2836d53de41def0a419f
Ramyadevi - web <ramyadevim7@gmail.com>
2025-02-13 07:07:36 -0800

    Imported bootstrap css in main.jsx and removed from App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import {BrowserRouter, Routes,Route} from 'react-router-dom'
 import { Navigate } from 'react-router-dom';
